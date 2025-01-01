c0d4dcff81adbee2717cd5e354541c5ea3e21db8
Sven Malvik <sven.malvik@vipps.no>
2025-01-01 14:30:34 +0100

    gh

--- .eleventy.js
+++ .eleventy.js
@@ -17,7 +17,7 @@ module.exports = function (eleventyConfig) {
   return {
     dir: {
       input: "src",
-      output: "docs",
+      output: "_site",
       includes: "_includes",
       data: "_data",
     },
